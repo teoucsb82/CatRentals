7fdc8f2fce423d852ef209a756f046686b2fd3ef
Andrew Richards <drew.rchrds@gmail.com>
2014-02-19 10:29:26 -0800

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CatsRental::Application.config.secret_key_base = '909e6de9acd6ffc62c52773933d189b318bb511756456bcddca7a8c88a69ff79848585f06b4f5fbb7f690c569a2bdca1684021be9ce405dc77bf27f7c066b0b0'
